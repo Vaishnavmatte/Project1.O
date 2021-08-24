0952dbbcec70737dac3ef40b2457bd093b12e1b3
Vaishnavmatte <89452568+Vaishnavmatte@users.noreply.github.com>
2021-08-24 15:12:17 +0530

    Add files via upload

---
+++ PROJECT 1.O.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[27]:
+
+
+#STUDENT DETAIL PROJECT#
+name=str(input('enter student name'))
+clas=int(input('enter the standard'))
+division=str(input('enter the division'))
+Rollno=int(input('enter the roll no'))
+Subject1=str(input('enter the subject1'))
+mark1=int(input('enter the marks of subject1'))
+Subject2=str(input('enter the subject2'))
+mark2=int(input('enter the mark of subject2'))
+print("Student Details")
+print("name: "+name)
+print("clas: ",clas)
+print("division: ",division)
+print("Rollno: ",Rollno)
+print("marks of: ",Subject1+" ",mark1)
+print("marks of: ",Subject2+" ",mark2)
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
